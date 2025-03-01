7ff22c672a4b051523573d070b0fbe541860ccb8
Ramon Cortez <acortez1993.09.15@outlook.com>
2025-03-01 08:54:18 -0700

    Done: se hizo el ruteo y se leyo la informacion del json en la repo. TODO: instalar una base de datos de sqlite con prisma y hacer codeFirst.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-const express = require("express");
+import express from "express";
+import usersRoutes from "./routes/users.routes.js";
 const app = express();
 const PORT = 3001;
-
-app.get("/", (req, res) => {
+//Middlewares
+app.use((req, res, next) => {
+  console.log("este middleware esta ejecutandose antes del get");
+  //Aqui agregariamos un middleware de autenticacion si es necesario
+  next();
+});
+app.use(express.json());
+app.use("/api", usersRoutes);
+app.get("/api", (req, res) => {
   return res.send("Hola desde Node js");
 });
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,2 +1,15 @@
-const { Router } = require("express");
-//TODO: practicar el routing y los metodos HTTP
+import { Router } from "express";
+import fs from "fs/promises";
+
+const router = Router();
+const path = "./src/repo/data.json";
+//Aqui se agregan las peticiones
+router.get("/users", async (req, res) => {
+  const fileData = await fs.readFile(path, "utf-8");
+  const response = JSON.parse(fileData);
+  if (!response) {
+    return res.status(404).send("No hay datos de usuarios");
+  }
+  res.json(response);
+});
+export default router;
